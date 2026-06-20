d0dc3e66a652165b192b85fde56b1ca6ccb9437d
Babaacarsamb <babacarsamb3075@gmail.com>
2026-06-20 02:16:27 +0000

    ajout des parfums v4

--- script.js
+++ script.js
@@ -1,53 +1,9 @@
 const phone = "221710474235";
-
-
-//les noms des parfum ,prix ;
-// Oud pour classik ,25000 ,Unisexe
-// Rose bleu Maison massimo, 75000 ,Unisexe
-// Liquid brun ,Unisexe ,25000
-  // Voyage de nuit ,Maison massimo Unisexe 75000 ,VoyageDeNuit.jpeg
- 
-// Royal oak maison massimo Unisexe 75000 ,Royal_oak_maison_massimo .jpeg
-//          Oud Madawi 150000   Unisexe   oudMadawi.jpeg
-// Coeur blanc maison massimo    Unisexe 75000              Coeur_blanc_maison_massimo .jpeg
-// Exotic    Unisexe 16000             Exotic.jpeg
-
-//    Taraf arabian oud 150000   Unisexe                        tarafArabianOud.jpeg
-
-
-// Africain gold Maison calisto Unisexe 90000 Africain_gold.jpeg
-
-// Suculent caramel 25000 Unisexe  Suculent_caramel.jpeg
-
-//    Eclair latafa Unisexe  23000  Eclair_latafa.jpeg
-//  Oud envoutant  homme , 23000 , Oud_envoutant .jpeg 
-
-  //   latafa 23000 Femme  Eclair_latafa.jpeg
-
-//   club_de_nuit.jpeg   Club de nuit 25000 Homme
-//   Femme Marshmallow 20000 Femme Marshmallow.jpeg
-
-// Gold signature 75000 Unisexe Gold_signature.jpeg
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // git add .
 // git commit -m "feat: ajout des parfums v3"
 // git push origin main
-
-
 //d: et :b  ne complete par rien laisse les comme ils sont
+
 const products = [
 {n:"Oud pour classik", c:"unisexe", p:"25 000", i:"Prouduits/oudPourKlassik.jpeg", d:"Classik Paris", b:"Collection Oud"},
 {n:"Rose bleu Maison massimo", c:"unisexe", p:"75 000", i:"Prouduits/laRoseBleu.jpeg", d:"Maison Massimo", b:"Collection Prestige"},
@@ -60,48 +16,25 @@ const products = [
 {n:"Taraf arabian oud", c:"unisexe", p:"150 000", i:"Prouduits/tarafArabianOud.jpeg", d:"Arabian Oud", b:"Collection Royale"},,
 {n:"Africain gold ", c:"unisexe", p:"90 000", i:"Prouduits/Africain_gold.jpeg", d:"Audacieux", b:""},
 {n:"Suculent caramel ", c:"unisexe", p:"25 000", i:"Prouduits/Suculent_caramel.jpeg", d:"Suculent", b:""},
-{n:"Eclair latafa ", c:"femme", p:"23 000", i:"Prouduits/Eclair_latafa.jpeg", d:"Eclair", b:""},
     {n:"Oud envoutant ", c:"unisexe", p:"23 000", i:"Prouduits/Oud_envoutant .jpeg", d:"Oud", b:""},
-    {n:"Club de nuit ", c:"homme", p:"25 000", i:"Prouduits/Club_de_nuit.jpeg", d:"Floral", b:""},
+    {n:"Club de nuit ", c:"homme", p:"25 000", i:"Prouduits/club_de_nuit.jpeg", d:"Floral", b:""},
     {n:"Marshmallow", c:"femme", p:"20 000", i:"Prouduits/Marshmallow.jpeg", d:"Sophistiqué", b:""},
     {n:"Gold signature ", c:"unisexe", p:"75000", i:"Prouduits/Gold_signature.jpeg", d:"Explosion", b:""},
     {n:"Rifaqat ", c:"unisexe", p:"15 000", i:"Prouduits/Rifaqat.jpeg", d:"Raffiné", b:""},
     {n:"Intense wayfarer ", c:"homme", p:"15 000", i:"Prouduits/Intense_wayfarer.jpeg", d:"Lumineux", b:""},
-    {n:"Terre d'Hermès", c:"homme", p:"46 000", i:"https://images.unsplash.com/photo-1595425970377-c9703cf48b6d", d:"Boisé", b:"Luxe"},
-    {n:"Burberry Her", c:"femme", p:"39 500", i:"https://i.pinimg.com/1200x/40/6c/d3/406cd31536bba98bc4cc07ca7f09492b.jpg", d:"Gourmand", b:""},
-    {n:"Good Girl", c:"femme", p:"41 500", i:"https://i.pinimg.com/1200x/b0/21/fa/b021fad89807037767df4d9e9af51dfd.jpg", d:"Audacieux", b:"Best Seller"},
-    {n:"Azzaro Wanted", c:"homme", p:"33 000", i:"https://i.pinimg.com/1200x/1c/85/70/1c8570df35e06d6bbb25607d762ac863.jpg", d:"Aventure", b:""},
-    {n:"Gypsy Water", c:"unisexe", p:"52 000", i:"https://images.unsplash.com/photo-1557170334-a9632e77c6e4", d:"Libre", b:"Luxe"},
-    {n:"Jo Malone Wood Sage", c:"unisexe", p:"48 000", i:"https://i.pinimg.com/1200x/02/da/77/02da77fd10adeca202ffe56fbb8068c1.jpg", d:"Boisé", b:""},
-    {n:"Yves Saint Laurent Libre", c:"femme", p:"42 500", i:"https://i.pinimg.com/736x/3e/39/c9/3e39c9086f59c3100d59872b411dc7a2.jpg", d:"Liberté", b:"Best Seller"},
-    {n:"Bleu de Chanel", c:"homme", p:"47 000", i:"https://i.pinimg.com/736x/1d/a4/6d/1da46dcb115adabdda5b99c6e9fd3475.jpg", d:"Frais et intense", b:""},
-    {n:"Tom Ford Black Orchid", c:"unisexe", p:"60 000", i:"https://i.pinimg.com/1200x/b2/da/2c/b2da2c878ebf0b9283225715e1090dc9.jpg", d:"Mystérieux", b:"Luxe"},
-    {n:"Chloé Eau de Parfum", c:"femme", p:"40 500", i:"https://i.pinimg.com/1200x/7e/a5/dd/7ea5dd90adcf9ba8ad392f451f650b2f.jpg", d:"Romantique", b:""},
-    {n:"Hugo Boss Bottled", c:"homme", p:"37 500", i:"https://i.pinimg.com/1200x/3f/b8/5d/3fb85d3c63e7d36598e25fbbb28ea255.jpg", d:"Classique", b:""},
-    {n:"Narciso Rodriguez For Her", c:"femme", p:"43 000", i:"https://i.pinimg.com/736x/6b/ab/7b/6bab7b9165796ad5a921791514076ea5.jpg", d:"Sensuel", b:""},
-    {n:"Dolce & Gabbana Light Blue", c:"femme", p:"38 500", i:"https://i.pinimg.com/1200x/2f/eb/48/2feb48f83ffae354b2513a7c2eaec42c.jpg", d:"Frais et fruité", b:""},
-    {n:"Versace Dylan Blue", c:"homme", p:"44 500", i:"https://i.pinimg.com/1200x/9a/75/24/9a752461dae05d3314ee90c213c127af.jpg", d:"Charismatique", b:""},
-    {n:"Calvin Klein Euphoria", c:"femme", p:"39 000", i:"https://i.pinimg.com/736x/e3/0d/e3/e30de3ca006a7c1d62b5f0854c12e653.jpg", d:"Mystérieuse", b:""},
-    {n:"Givenchy Gentleman", c:"homme", p:"41 000", i:"https://i.pinimg.com/736x/21/4f/7d/214f7dfbef4d9648a1a942582032eab1.jpg", d:"Élégance", b:""},
-    {n:"Lancôme La Nuit Tresor", c:"femme", p:"45 000", i:"https://i.pinimg.com/1200x/dd/be/d0/ddbed07dfabe90d12dcf5ec1426bd728.jpg", d:"Romantique", b:"Best Seller"},
-    {n:"Paco Rabanne Invictus", c:"homme", p:"39 500", i:"https://i.pinimg.com/736x/c6/bd/fb/c6bdfb8d752a231d524f930da032f675.jpg", d:"Vainqueur", b:""},
-    {n:"Bvlgari Omnia", c:"femme", p:"37 500", i:"https://i.pinimg.com/1200x/25/76/42/2576426a9053f3a854acacc0dd31d34f.jpg", d:"Éclatant", b:""},
-    {n:"Hermès Un Jardin Sur Le Nil", c:"unisexe", p:"50 000", i:"https://i.pinimg.com/736x/85/1d/61/851d61c4906b033e9ff84214a9f67dfc.jpg", d:"Naturel", b:"Luxe"},
-    {n:"Jean Paul Gaultier Scandal", c:"femme", p:"44 500", i:"https://i.pinimg.com/736x/6a/61/5b/6a615bb430b5b17c6f778cd5589b4f23.jpg", d:"Provocante", b:""},
-    {n:"Aventus Creed", c:"homme", p:"120 000", i:"https://i.pinimg.com/736x/4f/f2/31/4ff231f98a5a5e0613c062ed7710eac4.jpg", d:"Puissant", b:"Luxe"},
-    {n:"Miss Dior", c:"femme", p:"42 000", i:"https://i.pinimg.com/736x/32/4e/2d/324e2db21d282d323a4602f03a140286.jpg", d:"Romantique", b:"Best Seller"},
-    {n:"Mont Blanc Legend", c:"homme", p:"36 500", i:"https://i.pinimg.com/1200x/ef/d1/56/efd156127ff36f2ce9f2bf105bf53057.jpg", d:"Classique", b:""},
-    {n:"Tommy Girl", c:"femme", p:"35 000", i:"https://i.pinimg.com/1200x/1c/cf/97/1ccf97081c6adb10321dfe7e916c0f85.jpg", d:"Fraîche", b:""},
-    {n:"Viktor & Rolf Spicebomb", c:"homme", p:"43 500", i:"https://i.pinimg.com/736x/b6/8a/05/b68a0533b5d18ccf3ceb2386eda24797.jpg", d:"Épicé", b:""},
-    {n:"Elie Saab Le Parfum", c:"femme", p:"46 000", i:"https://i.pinimg.com/736x/da/37/0b/da370bdfa60cdb6ee71da2c0a7e734e0.jpg", d:"Floral et sucré", b:"Best Seller"},
-    {n:"Dunhill Icon", c:"homme", p:"38 000", i:"https://i.pinimg.com/1200x/a4/d1/f8/a4d1f821a5094936a34cd304b9484dbb.jpg", d:"Élégant", b:""},
-    {n:"Carolina Herrera Good Girl Legere", c:"femme", p:"41 000", i:"https://i.pinimg.com/736x/d9/e1/96/d9e1967c3a16a5821ac65be1c94eed7c.jpg", d:"Audacieuse", b:""},
-    {n:"Creed Green Irish Tweed", c:"homme", p:"115 000", i:"https://i.pinimg.com/736x/60/cf/07/60cf07782461d372c7e8daca4c596256.jpg", d:"Frais et classique", b:"Luxe"},
-    {n:"Chanel Chance", c:"femme", p:"43 500", i:"https://i.pinimg.com/736x/e0/e7/27/e0e72726db63eb25490dd0d6f5c5e895.jpg", d:"Chanceux", b:"Best Seller"},
-    {n:"Dolce & Gabbana The One", c:"homme", p:"42 000", i:"https://i.pinimg.com/736x/b5/30/88/b5308848d37e54808240c2fe1af62127.jpg", d:"Séduisant", b:""},
-    {n:"Jo Malone Peony & Blush Suede", c:"femme", p:"47 500", i:"https://i.pinimg.com/1200x/e4/08/e7/e408e7d8f95afefb72d6156b04356ea0.jpg", d:"Floral", b:""},
-    {n:"Maison Margiela Replica Jazz Club", c:"homme", p:"50 000", i:"https://i.pinimg.com/1200x/5c/93/37/5c93374de64519069d63b63fb4829ae4.jpg", d:"Boisé et fumé", b:"Luxe"},
-    ];
+    {n:"Azzure royal", c:"Unisexe", p:"25 000", i:"Prouduits/Azzure_royal.jpg", d:"Boisé", b:"Luxe"},
+    {n:"Momento riffs ", c:"Unisexe", p:"25 000", i:"Prouduits/Momento_riffs .jpg", d:"Gourmand", b:""},
+    {n:"Hawas for him", c:"Unisexe", p:"30 000", i:"Prouduits/Hawas_for_him.jpg", d:"Audacieux", b:"Best Seller"},
+    {n:"Sultan 9", c:"Unisexe", p:"25 000", i:"Prouduits/Sultan_9.jpg", d:"Aventure", b:""},
+    {n:"Eshal vanilla ", c:"femme", p:"20 000", i:"Prouduits/Eshal_vanilla.jpeg", d:"Libre", b:"Luxe"},
+    {n:"Vanilla seduction", c:"femme", p:"25 000", i:"Prouduits/Vanilla_seduction.jpeg", d:"Boisé", b:""},
+    {n:"Khair Confection", c:"femme", p:"15 000", i:"Prouduits/khair_confection.jpg", d:"Gourmand", b:"Nouveau"},
+    {n:"Khair Fusion", c:"femme", p:"15 000", i:"Prouduits/khair_fusion.jpg", d:"Fruité", b:"Nouveau"},  
+    {n:"Khair Pistachio", c:"femme", p:"15 000", i:"Prouduits/khair_pistachio.jpg", d:"Pistache", b:"Best Seller"},
+    {n:"Ignite Oud", c:"unisexe", p:"25 000", i:"Prouduits/Ignite_oud.jpg", d:"Boisé", b:"Collection Oud"},
+    {n:"Eclaire Latafa", c:"femme", p:"23 000", i:"Prouduits/Eclaire_latafa.jpeg", d:"Vanillé", b:"Nouveau"}
+   
+       ];
 
 const catIcons = {
   femme: 'fa-venus',
